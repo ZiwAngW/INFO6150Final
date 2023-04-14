9b89cb7c5fae675218062b3244fc90938adb7825
Ziwen Wang <ziwangw@gmail.com>
2023-04-14 19:54:10 -0400

    added functionality to logout fix navigate page error in login add page for register

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,20 +1,25 @@
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 const Navbar = () => {
   const { user } = useContext(AuthContext);
-
+  let nav = useNavigate()
+  const toLogin = () => {
+    nav(`../login`)
+  }
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">lamabooking</span>
         </Link>
-        {user ? user.username : (
+        {user ? <div>{user.username}
+            <button className="navButton">sign out</button>
+        </div> : (
           <div className="navItems">
             <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+            <button className="navButton" onClick={toLogin}>Login</button>
           </div>
         )}
       </div>
