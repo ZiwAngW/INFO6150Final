f0703d4b3b6a7797bdfacac23ce0c5b9adbc8a72
Faizan <sentry88prime@gmail.com>
2023-04-16 21:58:36 -0400

    added api to get hotel by type

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -10,7 +10,8 @@ import {
   updateHotel,
 } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-import {verifyAdmin} from "../utils/verifyToken.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
+
 const router = express.Router();
 
 //CREATE
@@ -18,16 +19,28 @@ router.post("/", verifyAdmin, createHotel);
 
 //UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
+
 //DELETE
 router.delete("/:id", verifyAdmin, deleteHotel);
+
 //GET
-
 router.get("/find/:id", getHotel);
-//GET ALL
 
+//GET ALL
 router.get("/", getHotels);
 router.get("/countByCity", countByCity);
 router.get("/countByType", countByType);
 router.get("/room/:id", getHotelRooms);
+
+//GET HOTELS BY TYPE
+router.get("/type/:type", async (req, res, next) => {
+  const type = req.params.type;
+  try {
+    const hotels = await Hotel.find({ type: type });
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
