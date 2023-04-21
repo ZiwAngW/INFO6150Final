1bc1bdf52b2941c5eb7db2ab5d1c6025fbe0bc31
Adarsh Gupta <adarshguptamaurya@gmail.com>
2023-04-20 20:06:36 -0400

    removed graph and semi updated the table

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -6,7 +6,11 @@ import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
 import Table from "../../components/table/Table";
 
+
+
 const Home = () => {
+
+
   return (
     <div className="home">
       <Sidebar />
@@ -18,10 +22,10 @@ const Home = () => {
           <Widget type="earning" />
           <Widget type="balance" />
         </div>
-        <div className="charts">
+        {/* <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-        </div>
+        </div> */}
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
           <Table />
