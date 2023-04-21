ba84ead6246d377e9109f07e17b860d284323b37
Adarsh Gupta <adarshguptamaurya@gmail.com>
2023-04-20 21:00:49 -0400

    made table dynamic

--- admin/src/components/table/Table.jsx
+++ admin/src/components/table/Table.jsx
@@ -8,89 +8,58 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import axios from "axios";
 import useFetch from "../../hooks/useFetch";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+
 const List = () => {
   const { data, loading, error } = useFetch("/hotels/bookings");
-  const rows = [
-    {
-      id: 1143155,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 785,
-      status: "Approved",
-    },
-    {
-      id: 2235235,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Michael Doe",
-      date: "1 March",
-      amount: 900,
-      status: "Pending",
-    },
-    {
-      id: 2342353,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 35,
-      status: "Pending",
-    },
-    {
-      id: 2357741,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Jane Smith",
-      date: "1 March",
-      amount: 920,
-      status: "Approved",
-    },
-    {
-      id: 2342355,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Harold Carol",
-      date: "1 March",
-      amount: 2000,
-      status: "Pending",
-    },
-  ];
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString([], options);
+  };
+
+  if (!loading) {
+    console.log("THIS IS DATAAAA", data.data);
+  }
+
   return (
     <TableContainer component={Paper} className="table">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell className="tableCell">Booking ID</TableCell>
-            <TableCell className="tableCell">Hotel</TableCell>
+            <TableCell className="tableCell">Hotel Logo</TableCell>
+            <TableCell className="tableCell">Payment Method</TableCell>
             <TableCell className="tableCell">Customer</TableCell>
             <TableCell className="tableCell">Date</TableCell>
             <TableCell className="tableCell">Amount</TableCell>
-            {/* <TableCell className="tableCell">Payment Method</TableCell> */}
             <TableCell className="tableCell">Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">
-                <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
-                  {row.product}
-                </div>
-              </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
-              <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
-              {/* <TableCell className="tableCell">{row.method}</TableCell> */}
-              <TableCell className="tableCell">
-                <span className={`status ${row.status}`}>{row.status}</span>
-              </TableCell>
-            </TableRow>
-          ))}
+          {loading ? (
+            "loading"
+          ) : (
+            data.data &&
+            data.data.map((row) => (
+              <TableRow key={row._id}>
+                <TableCell className="tableCell">{row._id}</TableCell>
+                <TableCell className="tableCell">
+                  <div className="cellWrapper">
+                    <img src={row.hotel.photos[0]} alt="" className="image" />
+                    {row.product}
+                  </div>
+                </TableCell>
+                <TableCell className="tableCell">{row.hotel.name}</TableCell>
+                <TableCell className="tableCell">{row.user.username}</TableCell>
+                <TableCell className="tableCell">{formatDate(row.createdAt)}</TableCell>
+                <TableCell className="tableCell">{row.room.price}</TableCell>
+                <TableCell className="tableCell">
+                  <span className={`status ${row.status}`}>{row.status}</span>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
         </TableBody>
       </Table>
     </TableContainer>
