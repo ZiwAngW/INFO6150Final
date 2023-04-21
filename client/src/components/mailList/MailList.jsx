0d4ae91bd9b779734535bcec6c10dddbf9d0f325
Faizan <sentry88prime@gmail.com>
2023-04-20 22:03:20 -0400

    mail subscribe event handled

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,16 +1,27 @@
-import "./mailList.css"
+import React, { useState } from 'react';
+import './mailList.css';
 
 const MailList = () => {
+  const [subscribed, setSubscribed] = useState(false);
+
+  const handleSubscribe = () => {
+    setSubscribed(true);
+  };
+
   return (
     <div className="mail">
       <h1 className="mailTitle">Stay in touch!</h1>
       <span className="mailDesc">Sign up and we'll send the best deals to you</span>
-      <div className="mailInputContainer">
-        <input type="text" placeholder="Your Email" />
-        <button>Subscribe</button>
-      </div>
+      {subscribed ? (
+        <div className="mailMessage">You are subscribed!</div>
+      ) : (
+        <div className="mailInputContainer">
+          <input type="text" placeholder="Your Email" />
+          <button onClick={handleSubscribe}>Subscribe</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MailList
+export default MailList;
